75bc48a0828163816057b1049dcbad84f1caf7d6
BirkasTivadar <birkastivadar@gmail.com>
2021-06-26 20:11:16 +0200

    Locations feladat Spring Beanek Kész

--- locations-spring-solution/locations/src/main/java/locations/LocationsController.java
+++ locations-spring-solution/locations/src/main/java/locations/LocationsController.java
@@ -8,18 +8,20 @@ import java.util.List;
 @RestController
 public class LocationsController {
 
-    private List<Location> locations = List.of(
-            new Location(1001, "Pálffy terasz", 47.688312684390965, 17.63382643461122),
-            new Location(1002, "Sol Instituto", 36.719359297592376, -4.364970322146287)
-    );
+    private LocationsService locationsService;
+
+    public LocationsController(LocationsService locationsService) {
+        this.locationsService = locationsService;
+    }
 
     @GetMapping("/locations")
-    public String getLocations(){
-        StringBuilder sb = new StringBuilder();
-        locations.forEach(
-                e-> sb.append(e)
-                .append(" ")
-        );
-        return sb.toString();
+    public List<Location> getLocations() {
+        return locationsService.getLocations();
+//        StringBuilder sb = new StringBuilder();
+//        locationsService.getLocations().forEach(
+//                e -> sb.append(e)
+//                        .append(" ")
+//        );
+//        return sb.toString();
     }
 }
