6fd9fc0311b811695c81176c9ac13c997816bd1e
BirkasTivadar <birkastivadar@gmail.com>
2021-07-01 21:10:33 +0200

    REST webszolgáltatások REST webszolgáltatások Integrációs tesztelés - RestTemplate gyakorlás

--- oktatas/employees/employees/src/main/java/employees/EmployeesService.java
+++ oktatas/employees/employees/src/main/java/employees/EmployeesService.java
@@ -66,4 +66,9 @@ public class EmployeesService {
                 .orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id));
         employees.remove(employee);
     }
+
+    public void deleteAllEmployees() {
+        idGenerator = new AtomicLong();
+        employees.clear();
+    }
 }
