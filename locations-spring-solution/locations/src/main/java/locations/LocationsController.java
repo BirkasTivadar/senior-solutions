295be14d1cf745db3bebc5f999944b0187e52f67
BirkasTivadar <birkastivadar@gmail.com>
2021-07-03 21:55:24 +0200

    Locations feladat Validáció Beépített validációk Kész

--- locations-spring-solution/locations/src/main/java/locations/LocationsController.java
+++ locations-spring-solution/locations/src/main/java/locations/LocationsController.java
@@ -3,13 +3,16 @@ package locations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/locations")
@@ -43,12 +46,12 @@ public class LocationsController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public LocationDto createLocation(@RequestBody CreateLocationCommand command) {
+    public LocationDto createLocation(@Valid @RequestBody CreateLocationCommand command) {
         return locationsService.createLocation(command);
     }
 
     @PutMapping("/{id}")
-    public LocationDto updateLocation(@PathVariable("id") long id, @RequestBody UpdateLocationCommand command) {
+    public LocationDto updateLocation(@PathVariable("id") long id, @Valid @RequestBody UpdateLocationCommand command) {
         return locationsService.updateLocation(id, command);
     }
 
@@ -74,4 +77,28 @@ public class LocationsController {
                 .contentType(MediaType.APPLICATION_PROBLEM_JSON)
                 .body(problem);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Problem> handleValidException(MethodArgumentNotValidException exception) {
+        List<Violation> violations =
+                exception.getBindingResult().getFieldErrors().stream()
+                        .map(fe -> new Violation(fe.getField(), fe.getDefaultMessage()))
+                        .collect(Collectors.toList());
+
+        Problem problem = Problem.builder()
+                .withType(URI.create("locations/not-valid"))
+                .withTitle("Validation error")
+                .withStatus(Status.BAD_REQUEST)
+                .withDetail(exception.getMessage())
+                .with("violations", violations)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
+
+    }
+
+
 }
