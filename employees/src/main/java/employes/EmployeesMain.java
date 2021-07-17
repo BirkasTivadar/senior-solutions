6116328a9ea5fe920dfef625fa984f927af2d6ac
BirkasTivadar <birkastivadar@gmail.com>
2021-07-17 09:44:58 +0200

    Adatbázis programozás Java nyelven - JPA További egyszerű műveletek JPA-val - gyakorlat

--- employees/src/main/java/employes/EmployeesMain.java
+++ employees/src/main/java/employes/EmployeesMain.java
@@ -3,6 +3,7 @@ package employes;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class EmployeesMain {
 
@@ -11,9 +12,38 @@ public class EmployeesMain {
         EntityManager em = factory.createEntityManager();
         em.getTransaction().begin();
 
-        em.persist(new Employee("John Doe"));
+        Employee employee = new Employee("John Doe");
+
+        em.persist(employee);
+
+//        em.persist(new Employee("bibi"));
 
         em.getTransaction().commit();
+
+        System.out.println(employee.getId());
+
+        long id = employee.getId();
+
+        employee = em.find(Employee.class, id);
+        System.out.println(employee);
+
+        em.getTransaction().begin();
+        employee = em.find(Employee.class, id);
+        employee.setName("John John Doe");
+        em.getTransaction().commit();
+
+        List<Employee> employees = em.createQuery("select e from Employee e", Employee.class)
+                .getResultList();
+        System.out.println(employees);
+
+        em.getTransaction().begin();
+        employee = em.find(Employee.class, id);
+        em.remove(employee);
+        em.getTransaction().commit();
+
+        employees = em.createQuery("select e from Employee e", Employee.class)
+                .getResultList();
+        System.out.println(employees);
 
         em.close();
         factory.close();
