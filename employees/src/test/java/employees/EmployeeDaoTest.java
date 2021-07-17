4080bdeaeeef2135f0b7fb5b9e9c24de9b844134
BirkasTivadar <birkastivadar@gmail.com>
2021-07-17 16:36:33 +0200

    Adatbázis programozás Java nyelven - JPA Architektúra és integrációs tesztelés Flyway-el

--- employees/src/test/java/employees/EmployeeDaoTest.java
+++ employees/src/test/java/employees/EmployeeDaoTest.java
@@ -1,6 +1,8 @@
 package employees;
 
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +24,16 @@ public class EmployeeDaoTest {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu");
 
         employeeDao = new EmployeeDao(entityManagerFactory);
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/employees?useUnicode=true");
+        dataSource.setUser("employees");
+        dataSource.setPassword("employees");
+
+        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+
+        flyway.clean();
+        flyway.migrate();
     }
 
     @Test
