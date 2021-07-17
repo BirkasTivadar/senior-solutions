c8709932e8605178c6fdabb9336f277205b5f45c
BirkasTivadar <birkastivadar@gmail.com>
2021-07-17 11:19:43 +0200

    Adatbázis programozás Java nyelven - JPA Architektúra és integrációs tesztelés

--- employees/src/main/java/employees/EmployeesMain.java
+++ employees/src/main/java/employees/EmployeesMain.java
@@ -1,4 +1,4 @@
-package employes;
+package employees;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
---
+++ employees/src/test/java/employees/EmployeeDaoTest.java
@@ -0,0 +1,76 @@
+package employees;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeeDaoTest {
+
+    private EmployeeDao employeeDao;
+
+    @BeforeEach
+    public void init() {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu");
+
+        employeeDao = new EmployeeDao(entityManagerFactory);
+    }
+
+    @Test
+    void testSaveThenFindId() {
+        Employee employee = new Employee("John Doe");
+        employeeDao.save(employee);
+
+        long id = employee.getId();
+
+        Employee another = employeeDao.findById(id);
+
+        assertEquals("John Doe", another.getName());
+    }
+
+    @Test
+    void testSaveThenListAll() {
+        employeeDao.save(new Employee("John Doe"));
+        employeeDao.save(new Employee("Jane Doe"));
+
+        List<Employee> employees = employeeDao.listAll();
+
+        assertEquals(2, employees.size());
+        assertEquals(List.of("Jane Doe", "John Doe"), employees.stream().map(Employee::getName).collect(Collectors.toList()));
+    }
+
+    @Test
+    void testChangeName() {
+        Employee employee = new Employee("John Doe");
+        employeeDao.save(employee);
+
+        long id = employee.getId();
+        employeeDao.changeName(id, "Jane Doe");
+        employee = employeeDao.findById(id);
+
+        assertEquals("Jane Doe", employee.getName());
+
+    }
+
+    @Test
+    void testDelete(){
+        Employee employee = new Employee("John Doe");
+        employeeDao.save(employee);
+        long id = employee.getId();
+        employeeDao.delete(id);
+
+        List<Employee> employees = employeeDao.listAll();
+
+        assertTrue(employees.isEmpty());
+    }
+
+
+}
