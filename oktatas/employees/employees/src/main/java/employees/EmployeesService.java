7e17b42312d1b486b4195484d28f4529af3f946d
BirkasTivadar <birkastivadar@gmail.com>
2021-07-26 22:15:42 +0200

    Java Spring Boot Microservices Konfiguráció és naplózás Spring Boot naplózás Training360 videós gyakorlat

--- oktatas/employees/employees/src/main/java/employees/EmployeesService.java
+++ oktatas/employees/employees/src/main/java/employees/EmployeesService.java
@@ -1,7 +1,10 @@
 package employees;
 
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Type;
@@ -13,7 +16,11 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class EmployeesService {
+
+//    ha lombokot használok, és kiteszem @Slf4j annotációt
+//    private static final Logger log = LoggerFactory.getLogger(EmployeesService.class)
 
     private ModelMapper modelMapper;
 
@@ -45,6 +52,8 @@ public class EmployeesService {
     public EmployeeDto createEmployeeCommand(CreateEmployeeCommand command) {
         Employee employee = new Employee(idGenerator.incrementAndGet(), command.getName());
         employees.add(employee);
+        log.info("Employee has been created");
+        log.debug("Employee has been created with name {}", command.getName());
         return modelMapper.map(employee, EmployeeDto.class);
     }
 
