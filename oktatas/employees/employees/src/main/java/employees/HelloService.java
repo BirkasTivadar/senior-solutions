4bfdd8945555c224c374c7d5169132c01707d125
BirkasTivadar <birkastivadar@gmail.com>
2021-07-26 21:59:04 +0200

    Java Spring Boot Microservices Konfiguráció és naplózás Spring Boot konfiguráció Training360 videós gyakorlat

--- oktatas/employees/employees/src/main/java/employees/HelloService.java
+++ oktatas/employees/employees/src/main/java/employees/HelloService.java
@@ -1,14 +1,28 @@
 package employees;
 
 
+import lombok.AllArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@AllArgsConstructor
+@EnableConfigurationProperties(HelloProperties.class)
 public class HelloService {
 
+    private HelloProperties helloProperties;
+
+
+//    Akkor használjuk, ha nem csinálunk külön Properties osztályt
+//    public HelloService(@Value("${employees.message}") String message) {
+//        this.message = message;
+//    }
+
     public String sayHello() {
-        return "Hello Spring Boot (service devtools) " + LocalDateTime.now();
+
+        return helloProperties.getMessage() + " " + LocalDateTime.now();
     }
 }
