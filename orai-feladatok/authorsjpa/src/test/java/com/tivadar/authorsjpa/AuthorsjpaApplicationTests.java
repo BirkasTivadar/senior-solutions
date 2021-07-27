6008ad679c1e5ce7425b880934a9878419354589
BirkasTivadar <birkastivadar@gmail.com>
2021-07-27 16:40:10 +0200

    2021.07.27. Authors órai feladat Service és Repository kész?

---
+++ orai-feladatok/authorsjpa/src/test/java/com/tivadar/authorsjpa/AuthorsjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tivadar.authorsjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorsjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
