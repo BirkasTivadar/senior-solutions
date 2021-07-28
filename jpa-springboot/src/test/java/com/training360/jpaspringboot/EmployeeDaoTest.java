602a2d6b16f7aca7ffae8cd2e3e4339e767aeed9
BirkasTivadar <birkastivadar@gmail.com>
2021-07-28 15:20:11 +0200

    Adatbázis programozás Java nyelven - JPA JPA Spring Boottal gyakorlás videó alapján Javítva, nem kell a tesztbe a @ExtendWith(SpringExtension.class) annotáció, elég a @SpringBootTest

--- jpa-springboot/src/test/java/com/training360/jpaspringboot/EmployeeDaoTest.java
+++ jpa-springboot/src/test/java/com/training360/jpaspringboot/EmployeeDaoTest.java
@@ -1,15 +1,15 @@
 package com.training360.jpaspringboot;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class) // nem kell, elég a @SpringbootTest
 @SpringBootTest
 class EmployeeDaoTest {
 
