1be61509d859ea60fb12a2d4bb3144d41d3f3672
BirkasTivadar <birkastivadar@gmail.com>
2021-07-30 16:01:40 +0200

    2021.07.27. Authors órai feladat Kiegészítve Kristóf útmutatása alapján, önálló JpaRepository metódussal

--- orai-feladatok/authorsjpa/src/main/java/com/tivadar/authorsjpa/AuthorsRepository.java
+++ orai-feladatok/authorsjpa/src/main/java/com/tivadar/authorsjpa/AuthorsRepository.java
@@ -1,9 +1,11 @@
 package com.tivadar.authorsjpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.ManyToOne;
 import java.util.List;
 
 public interface AuthorsRepository extends JpaRepository<Author, Long> {
@@ -12,5 +14,9 @@ public interface AuthorsRepository extends JpaRepository<Author, Long> {
     List<Author> findAuthorsByPrefix(@Param("prefix") String prefix);
 
     List<Author> findAuthorByNameStartsWith(String prefix);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("update Author a set a.name=:name where a.id=:id")
+    void updateAuthorNameById(@Param("name") String name, @Param("id") Long id);
 }
 
