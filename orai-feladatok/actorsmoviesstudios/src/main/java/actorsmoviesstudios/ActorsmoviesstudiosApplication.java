5e9a3e0cda760c917b963335d3f7401659804923
BirkasTivadar <birkastivadar@gmail.com>
2021-08-08 13:33:55 +0200

    2021.08.03 órai feladat Start

---
+++ orai-feladatok/actorsmoviesstudios/src/main/java/actorsmoviesstudios/ActorsmoviesstudiosApplication.java
@@ -0,0 +1,13 @@
+package actorsmoviesstudios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsmoviesstudiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsmoviesstudiosApplication.class, args);
+	}
+
+}
---
+++ orai-feladatok/actorsmoviesstudios/src/test/java/actorsmoviesstudios/ActorsmoviesstudiosApplicationTests.java
@@ -0,0 +1,13 @@
+package actorsmoviesstudios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorsmoviesstudiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
