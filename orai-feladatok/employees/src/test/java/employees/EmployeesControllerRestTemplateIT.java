3713af7b3c0edf28274a575e1343a6a7834bfa54
BirkasTivadar <birkastivadar@gmail.com>
2021-08-24 20:01:06 +0200

    REST webszolgáltatások - Integrációs tesztelés ismétlés

--- orai-feladatok/employees/src/test/java/employees/EmployeesControllerRestTemplateIT.java
+++ orai-feladatok/employees/src/test/java/employees/EmployeesControllerRestTemplateIT.java
@@ -3,7 +3,6 @@ package employees;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
