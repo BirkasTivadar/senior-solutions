ea2b43969c3420fe24a68d1b4f92b6cacd56e12e
BirkasTivadar <birkastivadar@gmail.com>
2021-08-28 13:02:02 +0200

    Adatbázis programozás Java nyelven - JPA Architektúra és integrációs tesztelés ismétlés

---
+++ jpa/src/main/java/employees/EmployeeDao.java
@@ -0,0 +1,55 @@
+package employees;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class EmployeeDao {
+
+    private final EntityManagerFactory factory;
+
+    public EmployeeDao(EntityManagerFactory factory) {
+        this.factory = factory;
+    }
+
+    public void save(Employee employee) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(employee);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public Employee findById(Long id) {
+        EntityManager em = factory.createEntityManager();
+        Employee employee = em.find(Employee.class, id);
+        em.close();
+        return employee;
+    }
+
+    public List<Employee> listAll() {
+        EntityManager em = factory.createEntityManager();
+        List<Employee> employees = em.createQuery("select e from Employee e order by e.name", Employee.class)
+                .getResultList();
+        em.close();
+        return employees;
+    }
+
+    public void changeName(Long id, String name) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        Employee employee = em.find(Employee.class, id);
+        employee.setName(name);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void delete(Long id) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        Employee employee = em.getReference(Employee.class, id);
+        em.remove(employee);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
---
+++ jpa/src/test/java/employees/EmployeeDaoTest.java
@@ -0,0 +1,85 @@
+package employees;
+
+//import com.mysql.cj.jdbc.MysqlDataSource;
+//import org.flywaydb.core.Flyway;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeDaoTest {
+
+    private EmployeeDao employeeDao;
+
+    @BeforeEach
+    void init() {
+//        MysqlDataSource dataSource = new MysqlDataSource();
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/employees?useUnicode=true");
+//        dataSource.setUser("employees");
+//        dataSource.setPassword("employees");
+//
+//        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+//        flyway.clean();
+//        flyway.migrate();
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("pu");
+        employeeDao = new EmployeeDao(factory);
+    }
+
+    @Test
+    void testSaveThenFind() {
+        Employee employee = new Employee("John Doe");
+        employeeDao.save(employee);
+
+        long id = employee.getId();
+
+        Employee another = employeeDao.findById(id);
+
+        assertEquals("John Doe", another.getName());
+    }
+
+    @Test
+    void testSaveThenListAll() {
+        employeeDao.save(new Employee("John Doe"));
+        employeeDao.save(new Employee("Jane Doe"));
+
+        List<Employee> employees = employeeDao.listAll();
+
+        assertEquals(2, employees.size());
+        assertEquals("Jane Doe", employees.get(0).getName());
+
+        assertEquals(List.of("Jane Doe", "John Doe"), employees.stream().map(Employee::getName).toList());
+    }
+
+    @Test
+    void testChangeName() {
+        Employee employee = new Employee("John Doe");
+        employeeDao.save(employee);
+
+        long id = employee.getId();
+
+        employeeDao.changeName(id, "Jack Doe");
+
+        assertEquals("Jack Doe", employeeDao.findById(id).getName());
+    }
+
+    @Test
+    void testDelete(){
+        Employee employee = new Employee("John Doe");
+        employeeDao.save(employee);
+
+        long id = employee.getId();
+
+        employeeDao.delete(id);
+
+        List<Employee> employees = employeeDao.listAll();
+
+        assertTrue(employees.isEmpty());
+    }
+}
