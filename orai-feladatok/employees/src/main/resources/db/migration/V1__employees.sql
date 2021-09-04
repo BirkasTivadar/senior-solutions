43f082c2e77ac67cfa630f75b65a98a07d650dcc
BirkasTivadar <birkastivadar@gmail.com>
2021-09-04 09:58:25 +0200

    Adatbáziskezelés Séma inicializálás Flyway-jel - gyakorlat ismétlés

---
+++ orai-feladatok/employees/src/main/resources/db/migration/V1__employees.sql
@@ -0,0 +1,3 @@
+CREATE TABLE employees (id BIGINT AUTO_INCREMENT, emp_name VARCHAR(255), PRIMARY KEY (id));
+INSERT INTO employees(emp_name) VALUES ('John Doe');
+INSERT INTO employees(emp_name) VALUES ('Jack Doe');
