e6ebb63411d8ccacf638f20559439f65400ab96b
BirkasTivadar <birkastivadar@gmail.com>
2021-09-14 19:34:42 +0200

    sv2021-jvjbf-kepesitovizsga Másféle megoldás Recorder mentés WorldRecord mentés Kész

--- oktatas/sv2021-jvjbf-kepesitovizsga/src/test/java/training360/guinnessapp/WorldRecordSavingIT.java
+++ oktatas/sv2021-jvjbf-kepesitovizsga/src/test/java/training360/guinnessapp/WorldRecordSavingIT.java
@@ -8,47 +8,47 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
-//import training360.guinnessapp.dto.RecorderCreateCommand;
-//import training360.guinnessapp.dto.RecorderDto;
-//import training360.guinnessapp.dto.WorldRecordCreateCommand;
-//import training360.guinnessapp.dto.WorldRecordDto;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//import java.util.Map;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Sql(statements = {"delete from world_record", "delete from recorder"})
-//public class WorldRecordSavingIT {
-//
-//    @Autowired
-//    TestRestTemplate template;
-//
-//    long recorderId;
-//
-//    @BeforeEach
-//    void saveTestRecorder() {
-//        RecorderCreateCommand inputCommand = new RecorderCreateCommand("Ben", LocalDate.of(1999, 9, 9));
-//        RecorderDto recorder = template.postForObject("/api/recorders", inputCommand, RecorderDto.class);
-//        recorderId = recorder.getId();
-//    }
-//
-//    @Test
-//    void test_saveSuccessful() {
-//        WorldRecordCreateCommand inputCommand =
-//                new WorldRecordCreateCommand("Largest pizza", 5.78, "meter", LocalDate.of(2001, 11, 11), recorderId);
-//
-//        WorldRecordDto record = template.postForObject("/api/worldrecords", inputCommand, WorldRecordDto.class);
-//
-//        assertEquals("Largest pizza", record.getDescription());
-//        assertEquals(5.78, record.getValue(), 0.005);
-//        assertEquals("meter", record.getUnitOfMeasure());
-//        assertEquals(LocalDate.of(2001, 11, 11), record.getDateOfRecord());
-//        assertEquals("Ben", record.getRecorderName());
-//    }
-//
+import training360.guinnessapp.dto.RecorderCreateCommand;
+import training360.guinnessapp.dto.RecorderDto;
+import training360.guinnessapp.dto.WorldRecordCreateCommand;
+import training360.guinnessapp.dto.WorldRecordDto;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(statements = {"delete from world_record", "delete from recorder"})
+public class WorldRecordSavingIT {
+
+    @Autowired
+    TestRestTemplate template;
+
+    long recorderId;
+
+    @BeforeEach
+    void saveTestRecorder() {
+        RecorderCreateCommand inputCommand = new RecorderCreateCommand("Ben", LocalDate.of(1999, 9, 9));
+        RecorderDto recorder = template.postForObject("/api/recorders", inputCommand, RecorderDto.class);
+        recorderId = recorder.getId();
+    }
+
+    @Test
+    void test_saveSuccessful() {
+        WorldRecordCreateCommand inputCommand =
+                new WorldRecordCreateCommand("Largest pizza", 5.78, "meter", LocalDate.of(2001, 11, 11), recorderId);
+
+        WorldRecordDto record = template.postForObject("/api/worldrecords", inputCommand, WorldRecordDto.class);
+
+        assertEquals("Largest pizza", record.getDescription());
+        assertEquals(5.78, record.getValue(), 0.005);
+        assertEquals("meter", record.getUnitOfMeasure());
+        assertEquals(LocalDate.of(2001, 11, 11), record.getDateOfRecord());
+        assertEquals("Ben", record.getRecorderName());
+    }
+
 //    @Test
 //    void test_DescriptionBlank() {
 //        WorldRecordCreateCommand inputCommand =
@@ -68,4 +68,4 @@ import org.zalando.problem.Status;
 //        assertEquals(Status.NOT_FOUND, problem.getStatus());
 //        assertEquals("Recorder not found", problem.getTitle());
 //    }
-//}
+}
